7bfa936b320c0dd9896a8acf8d899b6f383b8427
Edward Chor <13307130149@fudan.edu.cn>
2018-07-05 22:41:46 +0800

    test on gpus

--- models/pt/cGAN.py
+++ models/pt/cGAN.py
@@ -102,14 +102,14 @@ class Generator(nn.Module):
         super(Generator,self).__init__()
 
         self.use_gpu=torch.cuda.is_available()
-
+        self.g_conv1=nn.Conv2d(4,16,3,1,1)
         self.z_conv1=nn.Conv2d(8,16,5,1,2)
         self.z_pool1=nn.MaxPool2d(2)
         self.z_conv2=nn.Conv2d(16,32,7,1,0)
-        self.z_conv3=nn.Conv2d(36,64,1,1,0)
+        self.z_conv3=nn.Conv2d(48,64,1,1,0)
         self.z_conv4=nn.Conv2d(64,128,3,1,1)
-        self.fc1=nn.Linear(128*10*10,6400)
-        self.fc2=nn.Linear(6400,56*56)
+        self.fc1=nn.Linear(128*10*10,56*56)
+        # self.fc2=nn.Linear(6400,56*56)
         self.lrelu=nn.LeakyReLU()
 
         self.bn1=nn.BatchNorm2d(32)
@@ -119,37 +119,39 @@ class Generator(nn.Module):
 
         if self.use_gpu:
             g=g.view(-1,4,10,10).cuda()
+            gc1=self.g_conv1(g)
             z=z.view(-1,8,32,32).cuda()
             z1=self.lrelu(self.z_pool1(self.z_conv1(z)))
             z2=self.lrelu(self.z_conv2(z1))
             z2=self.bn1(z2)
 
-            _z3=torch.cat([g,z2],1)
+            _z3=torch.cat([gc1,z2],1)
             z3=self.lrelu(self.z_conv3(_z3))
             z3=self.bn2(z3)
 
             z4=self.lrelu(self.z_conv4(z3))
             z4=z4.view(-1,128*10*10).cuda()
-            fc1=self.fc1(z4)
+            o=self.fc1(z4)
             
-            o = self.fc2(fc1)
+            # o = self.fc2(fc1)
             return o.view(-1,1,56,56).cuda()
         else:
             g=g.view(-1,4,10,10)
+            gc1 = self.g_conv1(g)
             z=z.view(-1,8,32,32)
             z1=self.lrelu(self.z_pool1(self.z_conv1(z)))
             z2=self.lrelu(self.z_conv2(z1))
             print("z2.shape: ",z2.shape)
             z2 = self.bn1(z2)
-            _z3=torch.cat([g,z2],1)
+            _z3=torch.cat([gc1,z2],1)
             z3=self.lrelu(self.z_conv3(_z3))
             z3 = self.bn2(z3)
             z4=self.lrelu(self.z_conv4(z3))
             print("z4.shape: ", z4.shape)
             z4=z4.view(-1,128*10*10)
-            fc1=self.fc1(z4)
-            print("fc1.shape: ", fc1.shape)
-            o = self.fc2(fc1)
+            o=self.fc1(z4)
+            # print("fc1.shape: ", fc1.shape)
+            # o = self.fc2(fc1)
             return o.view(-1,1,56,56)
 
 
@@ -244,7 +246,7 @@ class cDCGAN(nn.Module):
             self.G = self.G.cuda()
             self.D = self.D.cuda()
 
-        self.g_lr = 0.00001
+        self.g_lr = 0.0005
         self.d_lr=0.001
         self.batch_size = 25
         self.iters = 1000
@@ -287,8 +289,8 @@ class cDCGAN(nn.Module):
         return [_.cuda() for _ in l]
 
     def trainNetwork(self):
-        G_optimizer = optim.SGD(self.G.parameters(), lr=self.g_lr, momentum=0.9)
-        # G_optimizer= optim.Adadelta(self.G.parameters())
+        # G_optimizer = optim.SGD(self.G.parameters(), lr=self.g_lr, momentum=0.9)
+        G_optimizer= optim.Adadelta(self.G.parameters())
         D_optimizer = optim.SGD(self.D.parameters(), lr=self.d_lr, momentum=0.9)
         # D_optimizer=optim.Adadelta(self.D.parameters())
 
