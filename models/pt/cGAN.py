432a0e01e2775cabf092a4645157872e92121682
Edward Chor <13307130149@fudan.edu.cn>
2018-07-05 19:51:18 +0800

    d

--- models/pt/cGAN.py
+++ models/pt/cGAN.py
@@ -309,7 +309,7 @@ class cDCGAN(nn.Module):
 
 
 if __name__ == '__main__':
-    data = [np.load('../../data/doodle/G1000.npy'), np.load('../../data/doodle/I1000.npy')]
+    data = [np.load('../../data/doodle/G30000.npy'), np.load('../../data/doodle/I30000.npy')]
     dcgan = cDCGAN()
     dcgan.feedData(data)
 #    dcgan.loadCheckpoint('19')
